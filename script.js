4e0c9e474766fe87f56dee43eb72a34c246edfe3
Jonas Schulz <jonas.schulz@affinity.pt>
2018-12-30 14:27:16 +0000

    change h1 color to Germany flag

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function helloFunction() {
     alert("HelloJonas!");
+    document.getElementById("firstH1").style.color = "black";
+    document.getElementById("secondH1").style.color = "red";
+    document.getElementById("thirdH1").style.color = "yellow";
+
 }
